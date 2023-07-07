21b047d78801c6d31b3d9aaad2e7f56e461c76b4
Aleksey_Filkin <acid_wind@mail.ru>
2023-07-07 19:55:33 +0700

    program2 task solved

--- program2.java
+++ program2.java
@@ -1,5 +1,24 @@
+// Вывести все простые числа от 1 до 1000
+
 import java.util.Scanner;
 
 public class program2 {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Вывести все простые числа от 1 до ");
+        Integer n = in.nextInt();
+        in.close();
+                for (int i = 1; i < n+1; i++) {
+            Integer count = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i%j == 0) {
+                    count = count + 1;
+                }
+            }
+            if (count == 2){
+                System.out.print(i + ", ");
+            }
+        }
+    }
 }
